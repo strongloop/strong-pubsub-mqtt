9c8830a33ac8dfd6872d9c033cd3d545ec30349d
Ryan Graham <ryan@strongloop.com>
2015-07-20 17:40:52 -0700

    test: fix missing deps and refs
    Also use CI provided mosquitto when running on CI.

--- test/client.api.test.js
+++ test/client.api.test.js
@@ -2,12 +2,17 @@ var helpers = require('strong-pubsub-test');
 var getPort = helpers.getFreePort;
 var defineClientTests = helpers.defineClientTests;
 var usingMosquitto = helpers.usingMosquitto;
-var Client = require('../../strong-pubsub');
+var Client = require('strong-pubsub');
 var Adapter = require('../'); // strong-pubsub-mqtt
 
 describe('MQTT', function () {
   beforeEach(function(done) {
     var test = this;
+    if (process.env.CI) {
+      // CI already has mosquitto running on the default port
+      test.port = 1883;
+      return done();
+    }
     usingMosquitto(function(err, port) {
       test.port = port;
       done(err);
--- test/client.behavior.test.js
+++ test/client.behavior.test.js
@@ -1,5 +1,5 @@
-var Client = require('../../strong-pubsub');
-var Adapter = require('../../strong-pubsub-mqtt');
+var Client = require('strong-pubsub');
+var Adapter = require('../');
 var helpers = require('strong-pubsub-test');
 var defineClientBehaviorTests = helpers.defineClientBehaviorTests;
 var usingMosquitto = helpers.usingMosquitto;
@@ -7,6 +7,11 @@ var usingMosquitto = helpers.usingMosquitto;
 describe('mqtt client behavior', function () {
   beforeEach(function(done) {
     var test = this;
+    if (process.env.CI) {
+      // CI already has mosquitto running on the default port
+      test.port = 1883;
+      return done();
+    }
     usingMosquitto(function(err, port) {
       test.port = port;
       done(err);
